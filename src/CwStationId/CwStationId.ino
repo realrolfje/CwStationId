36ecc21703af5c55a38ad86f6ed865656be40446
Rolf <realrolfje@users.noreply.github.com>
2016-10-22 20:44:06 +0200

    A bit of cleanup

--- src/CwStationId/CwStationId.ino
+++ src/CwStationId/CwStationId.ino
@@ -1,31 +1,37 @@
-
-// Modeled after transmitters like this one:
-// https://youtu.be/V5Qkk2c4m9E
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
+/*
+ * Funky piece of code to transmit CW (morse code) with a twist.
+ * 
+ * Instead of going beep beep, this code makes "chirps", as heard
+ * in old radios, like this replica: https://youtu.be/V5Qkk2c4m9E
+ * 
+ * It's just for laughs. Enjoy.
+ */
 
 // Size of a dot
-int dotLoop = 6;
+const int dotLoop = 6;
 
 // Target tone
-int targetTone = 800;
+const int targetTone = 800;
 
 // c1 target when no tone
-unsigned long c1TargetTone = 1300;
+const unsigned long c1TargetTone = 1300;
 
 // c2 target when no tone
-unsigned long c2TargetTone = 400;
+const unsigned long c2TargetTone = 400;
 
 // The tone "after" C1 of the oscillator
-unsigned long c1Tone= c1TargetTone;
+unsigned long c1Tone = c1TargetTone;
 
 // The tone "after" C2 of the oscillator
-unsigned long c2Tone= c2TargetTone;
+unsigned long c2Tone = c2TargetTone;
+
+void setup() {
+  // Where we're going we don't need no setup.
+}
 
 void loop() {
+  // Sorry, no lookuptables here. Being lazy.
+  
   dot();  dash(); dash(); dot();          dashRest();
   dot();  dash();                         dashRest();
   dash(); dash(); dash(); dash(); dash(); dashRest();
@@ -33,8 +39,8 @@ void loop() {
   dash(); dash(); dot();                  dashRest();
   dash(); dash(); dot(); dot();           dashRest();
 
+  // Wait 4 minutes
   long again = millis() + 1000*60*4;
-
   while (millis() < again){
     dotRest();
   }
@@ -74,20 +80,21 @@ void dashRest(){
   }
 }
 
-int calculateTone(int targetTone) {
-   int c1Speed = 2;
-   int c2Speed = 300;
+unsigned int calculateTone(unsigned int targetTone) {
+   const int c1Speed = 2;
+   const int c2Speed = 300;
   
    if (targetTone == 0) {
+     // No sound, deplete the condensers.
      c1Tone = (c1Tone* (c1Speed -1) + c1TargetTone) /  c1Speed;  // Fast filter
-     c2Tone = (c2Tone* (c2Speed -1) + c2TargetTone) / c2Speed;  // Slow filter
+     c2Tone = (c2Tone* (c2Speed -1) + c2TargetTone) /  c2Speed;  // Slow filter
      
    } else {
+     // Sound, charge the condensers.
      c1Tone = (c1Tone* (c1Speed -1) + targetTone) /  c1Speed;  // Fast filter
-     c2Tone = (c2Tone* (c2Speed -1) + targetTone) / c2Speed;  // Slow filter
+     c2Tone = (c2Tone* (c2Speed -1) + targetTone) /  c2Speed;  // Slow filter
    }
   
-   unsigned int combined = c1Tone + c2Tone;
-   return combined / 2;
+   return (c1Tone + c2Tone) / 2;
 }
-
+ 
