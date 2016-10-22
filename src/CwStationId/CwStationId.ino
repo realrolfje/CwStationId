ca1f4f3f8d20fb9743b49a19f090cbef1faa6d2a
Rolf <realrolfje@users.noreply.github.com>
2016-10-22 21:02:39 +0200

    Added another link to a CW chirp video

--- src/CwStationId/CwStationId.ino
+++ src/CwStationId/CwStationId.ino
@@ -4,6 +4,8 @@
  * Instead of going beep beep, this code makes "chirps", as heard
  * in old radios, like this replica: https://youtu.be/V5Qkk2c4m9E
  * 
+ * Another, different, chirp: https://www.youtube.com/watch?v=Ls_Y_USteQQ
+ * 
  * It's just for laughs. Enjoy.
  */
 
